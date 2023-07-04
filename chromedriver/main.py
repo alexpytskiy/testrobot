5775a20507d2935eda7705b2ec8848ad8d011970
a.pytskiy <a_pytskiy@mail.ru>
2023-07-04 19:17:39 +0300

    commitall

--- chromedriver/main.py
+++ chromedriver/main.py
@@ -18,6 +18,7 @@ try:
     driver.get(url=url)
     time.sleep(5)
 
+    #вводим логин на Портале
     login_input = driver.find_element(By.CSS_SELECTOR, 'input[type="text" i]')
     login_input.click()
     login_input.clear()
@@ -25,6 +26,7 @@ try:
     time.sleep(2)
     print("login OK")
 
+    #Вводим пароль на Портале
     password_input = driver.find_element(By.CSS_SELECTOR, 'input[type="password" i]')
     password_input.click()
     password_input.clear()
@@ -32,27 +34,35 @@ try:
     time.sleep(2)
     print("password OK")
 
+    #Входим под логином и паролем на Портал
     login_button = driver.find_element(By.CSS_SELECTOR, 'button').click()
     time.sleep(5)
     print("auth OK")
 
+    #Жмем кнопку "Товародвижение аптек"
     report_button = driver.find_element(By.XPATH, "//*[contains(text(), 'Товародвижение аптек')]").click()
     time.sleep(5)
     print("report_button OK")
 
-    date_first = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div[2]/form/div[3]/div[1]/div/div/div/div/div/div[1]')
+    #Выбираем начальную дату в календаре
+    #date_first = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div[2]/form/div[3]/div[1]/div/div/div/div/div/div[1]')
+    date_first = driver.find_element(By.XPATH, "//div[@class='css-1wy0on6']")
     date_first.click()
-    #date_first.clear()
     time.sleep(5)
-    date_first.send_keys("20230101")
-    time.sleep(15)
 
-    date_second = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div[2]/form/div[3]/div[1]/div/div/div/div/div/div[2]')
-    date_second.click()
-    #date_second.clear()
+    date_first_2 = driver.find_element(By.XPATH, '//div[@data-layer-child="true"]')
     time.sleep(5)
-    date_second.send_keys("20230101")
-    time.sleep(15)
+    date_first_2.clear()
+    date_first_2.send_keys("20230101")
+    print("OK")
+    #date_first_2.clear()
+    #date_first_2.send_keys(Keys.BACKSPACE)
+    time.sleep(5)
+    # #date_first_2.send_keys(Keys.BACKSPACE)
+    # #date_first_2.send_keys("20230101")
+    # # date_first_2.send_keys("20230101")
+    # time.sleep(15)
+    # # date_first_2.send_keys(Keys.RETURN)
 
 except Exception as ex:
     print(ex)
