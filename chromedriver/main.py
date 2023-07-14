73b05d5f4740ed087e22fb2ffb3588e587c4c2ce
a.pytskiy <a_pytskiy@mail.ru>
2023-07-14 11:18:30 +0300

    commit!

--- chromedriver/main.py
+++ chromedriver/main.py
@@ -2,17 +2,20 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver import Keys
+from selenium.webdriver import Keys, ActionChains
 from auth_data import portal_password
 
 
 options = webdriver.ChromeOptions()
+
 
 url = "https://portal.esc.ru/"
 service = Service(executable_path = "L:\\ОТДЕЛЫ\ОТДЕЛ ФИЛИАЛОВ и ПРЕДСТАВИТЕЛЬСТВ\\Маркетинговый союз\\Отчёты\!Python projects\\test venv\\chromedriver\\chromedriver.exe")
 options = webdriver.ChromeOptions()
 driver = webdriver.Chrome(service=service, options=options)
 driver.maximize_window()
+
+actions = ActionChains(driver)
 
 try:
     driver.get(url=url)
@@ -45,24 +48,36 @@ try:
     print("report_button OK")
 
     #Выбираем начальную дату в календаре
-    #date_first = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div[2]/form/div[3]/div[1]/div/div/div/div/div/div[1]')
-    date_first = driver.find_element(By.XPATH, "//div[@class='css-1wy0on6']")
+    #date_first = driver.find_element(By.XPATH, "//div[@class='css-1wy0on6']")
+    #date_first = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div[2]/form/div[3]/div[1]/div/div/input')
+    date_first = driver.find_element(By.XPATH, "//div[@class='css-s1rjxz']")
     date_first.click()
     time.sleep(5)
+    print("выбрал календарик")
 
-    date_first_2 = driver.find_element(By.XPATH, "//input[@name type='hidden']")
-    time.sleep(5)
-    date_first_2.click()
-    #date_first_2.send_keys("20230101")
-    print("OK")
-    #date_first_2.clear()
-    #date_first_2.send_keys(Keys.BACKSPACE)
-    time.sleep(5)
-    # #date_first_2.send_keys(Keys.BACKSPACE)
-    # #date_first_2.send_keys("20230101")
-    # # date_first_2.send_keys("20230101")
-    # time.sleep(15)
-    # # date_first_2.send_keys(Keys.RETURN)
+    date_first_2 = driver.find_element(By.XPATH, '// *[ @ id = "react-select-2-input"]')
+    date_first_2.send_keys("20230505")
+
+    print("ввел дату")
+    time.sleep(10)
+
+    #стартовать формирование отчетности
+    #date_first_2.submit()
+
+    #// *[ @ id = "react-select-2-input"]
+    # react-select-2-input
+
+    # actions.send_keys(Keys.BACKSPACE)
+    # time.sleep(5)
+    # print("типа нажал backspace")
+
+    # date_first_2 = driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div[2]/form/div[3]/div[1]/div/div/div/div/div')
+    # date_first_2.click()
+    # time.sleep(5)
+
+
+
+    # print("OKOKOK")
 
 except Exception as ex:
     print(ex)
