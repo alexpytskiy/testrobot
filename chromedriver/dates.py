a460011e25ff2e4cde672e04a245eb365909751e
a.pytskiy <a_pytskiy@mail.ru>
2023-07-19 12:41:08 +0300

    commit!

--- chromedriver/dates.py
+++ chromedriver/dates.py
@@ -3,15 +3,5 @@ from datetime import timedelta
 
 date = dt.now() + timedelta(days = -3)
 date_2 = date.strftime('%Y-%m-%d')
-#print(date_2)
 
-
-
-#НЕ ПРЕОБРАЗОВЫВАЕТ ФОРМУЛУ В ЗНАЧЕНИЕ!!!!
-
-
-
-
-
-# #cell_2 = dt.strftime(cell, '%Y-%m-%d')
 
