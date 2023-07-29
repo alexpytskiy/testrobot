963e27b35cfac6c2b2f5b8618fc26d7145a30f2c
alexpytskiy <a_pytskiy@mail.ru>
2023-07-29 12:14:25 +0300

    commit from home

--- chromedriver/import_file.py
+++ chromedriver/import_file.py
@@ -2,23 +2,37 @@ import os
 import shutil
 import time
 
-path = "C:\\Users\\a.pytskiy\\Downloads"
+# path = "C:\\Users\\a.pytskiy\\Downloads"
 
 
 #ищем последний файл в папке загрузок
-filename = max([os.path.join(path, f) for f in os.listdir(path)], key=os.path.getmtime)
-print(filename)
+# filename = max([os.path.join(path, f) for f in os.listdir(path)], key=os.path.getmtime)
+# print(filename)
 
 #переименовываем файл
-rename_file = os.rename(filename, os.path.join(path, "123.xlsx"))
-new_filename = max([os.path.join(path, f) for f in os.listdir(path)], key=os.path.getmtime)
-print(new_filename)
+# rename_file = os.rename(filename, os.path.join(path, "123.xlsx"))
+# new_filename = max([os.path.join(path, f) for f in os.listdir(path)], key=os.path.getmtime)
+# print(new_filename)
 
 #перемещаем новый файл в нужную папку
-new_path = "L:\\ОТДЕЛЫ\\ОТДЕЛ ФИЛИАЛОВ и ПРЕДСТАВИТЕЛЬСТВ\\Маркетинговый союз\\Отчёты\\!Python projects\\выгрузки\\" + "123.xlsx"
-shutil.move(new_filename, new_path)
-time.sleep(5)
-print(new_path)
+# new_path = "L:\\ОТДЕЛЫ\\ОТДЕЛ ФИЛИАЛОВ и ПРЕДСТАВИТЕЛЬСТВ\\Маркетинговый союз\\Отчёты\\!Python projects\\выгрузки\\" + "123.xlsx"
+# shutil.move(new_filename, new_path)
+# time.sleep(5)
+# print(new_path)
+
+def import_last_file():
+    path = "C:\\Users\\a.pytskiy\\Downloads"
+    filename = max([os.path.join(path, f) for f in os.listdir(path)], key=os.path.getmtime)
+    print(filename)
+    rename_file = os.rename(filename, os.path.join(path, "123.xlsx"))
+    new_filename = max([os.path.join(path, f) for f in os.listdir(path)], key=os.path.getmtime)
+    print(new_filename)
+    new_path = "L:\\ОТДЕЛЫ\\ОТДЕЛ ФИЛИАЛОВ и ПРЕДСТАВИТЕЛЬСТВ\\Маркетинговый союз\\Отчёты\\!Python projects\\выгрузки\\" + "123.xlsx"
+    shutil.move(new_filename, new_path)
+    time.sleep(5)
+    print(new_path)
+
+
 
 
 
